2d53f7ec7407fe47d3c35b99bac366fdb272e677
Guilherme Rigon <guilherme@rigon.info>
2016-03-30 00:22:00 -0300

    03 ok

---
+++ 03/Rigon/03.c
@@ -0,0 +1,36 @@
+/*
+Escreva um programa para ler uma frase e um caractere. Sempre que o caractere
+lido aparecer na frase ele deve ser substituído por asterisco. Por exemplo se os
+valores fornecidos fossem para a frase: o dia esta nublado, e para o caractere: a,
+o programa deverá fornecer o seguinte resultado: o di* est* nubl*do. Altere
+também para trocar todas as letras E por número 3, por exemplo.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  int i, tam;
+  char caractere[2];
+  char frase[201];
+
+  fgets(caractere,2,stdin);
+	getchar();
+  fgets(frase,201,stdin);
+
+  tam = strlen(frase) - 1;
+
+  for(i=0;i<tam;i++){
+    if ( frase[i] == caractere[0]){
+      frase[i] = '*';
+    }
+    if ( frase[i] == 'e'){
+      frase[i] = '3';
+    }
+  }
+
+  printf("%s\n", frase);
+
+  return 0;
+}
