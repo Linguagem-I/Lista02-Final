4318883a2779da37cff5dfe913b760e5c9412418
LorenzoWF <lorenzo.wendt@outlook.com>
2016-04-14 19:23:55 -0300

    Lorenzo 04 - 3

--- 04/Lorenzo/3.c
+++ 04/Lorenzo/3.c
@@ -5,7 +5,10 @@ int convert(int dec){
     if (dec == 0) {
         return 0;
     }
-    
+    printf("Decimal = %d\n", dec);
+    printf("%d modulo de 2 + 10 = %d\n", dec, dec % 2 + 10);
+    printf("%d / 2 = %d\n", dec, dec / 2);
+    printf("Decimal / 2 %d\n\n", dec / 2);
 	return (dec % 2 + 10 * convert(dec / 2));
 }
 
@@ -19,7 +22,7 @@ int main(){
     
     binario = convert(num);
     
-    printf("%d", binario);
+    printf("%d\n", binario);
     
     return 0;
 }
