be8cc91af47c40daac19073320f77689a93075a4
Guilherme Rigon <guilherme@rigon.info>
2016-05-01 02:02:33 -0300

    atualizado

--- Trabalho/Rigon/10.c
+++ Trabalho/Rigon/10.c
@@ -9,37 +9,53 @@ b) A média dos salários;*/
 #include "stdio.h"
 #include "string.h"
 
-char *strupr(char *str)
-{
-  unsigned char *p = (unsigned char *)str;
-
-  while (*p) {
-     *p = toupper(*p);
-      p++;
-  }
-  return str;
-}
-
-char *strlwr(char *str)
-{
-  unsigned char *p = (unsigned char *)str;
-
-  while (*p) {
-     *p = tolower(*p);
-      p++;
-  }
-  return str;
-}
+// char *strupr(char *str)
+// {
+//   unsigned char *p = (unsigned char *)str;
+//
+//   while (*p) {
+//      *p = toupper(*p);
+//       p++;
+//   }
+//   return str;
+// }
+//
+// char *strlwr(char *str)
+// {
+//   unsigned char *p = (unsigned char *)str;
+//
+//   while (*p) {
+//      *p = tolower(*p);
+//       p++;
+//   }
+//   return str;
+// }
 
 int main(){
 
   char nome[100], maiorN[100], menorN[100];
   float salario, maiorS = 0, menorS = 0, media = 0;
-  int continua = 1, i = 0;
+  int continua = 1, i = 0, flag = 0, j, teste = 0;
 
   do {
-    printf("Digite o nome:\n");
-    scanf(" %[^\n]", nome);
+    printf("Digite o nome:\n"); // 97 122
+    for(i=0,flag = 0;flag == 0 && strcmp(nome, "0") != 0;i++){
+      gets(nome);
+      for(j=0; j < strlen(nome) && flag == 0;j++){
+        if ( ((nome[j] >= 65 && nome[j] <= 90) || (nome[j] >= 97 && nome[j] <= 122))  && nome[j+1] == '\0')
+          printf("nao aceito\n");
+        if (nome[j] == ' ' && nome[j+1] == '\0')
+          printf("nao aceito espaco\n");
+        if ( (nome[j] == ' ') && ((nome[j+1] >= 65 && nome[j+1] <= 90) || (nome[j+1] >= 97 && nome[j+1] <= 122))){
+            printf("Nome aceito!\0");
+            flag = 1;
+            break;
+        }
+      }
+    }
+    if ( strcmp(nome, "0") == 0 )
+      break;
+    // flag = 0;
     printf("Digite o salario:\n");
     scanf("%f", &salario);
 
@@ -62,9 +78,9 @@ int main(){
       strcpy(menorN, nome);
     }
 
-    printf("Digite 0 para encerrar o cadastrou, ou outro numero para seguir cadastrando\n");
-    scanf("%d", &continua);
-  } while( continua != 0);
+     printf("Digite 0 para encerrar o cadastrou, ou outro numero para seguir cadastrando\n");
+     scanf("%d", &continua);
+  } while(continua != 0);
 
   for(i=0 ; maiorN[i] != ' '; i++){ // transforma o primeiro nome em maiusculas
     maiorN[i] = toupper(maiorN[i]);
